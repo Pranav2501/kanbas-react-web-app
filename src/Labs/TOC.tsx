3ff73d3ceba94b6e733d952ca3621185b59e980d
Pranav2501 <96072640+Pranav2501@users.noreply.github.com>
2024-08-18 12:56:22 -0700

    react project

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -49,7 +49,7 @@ export default function TOC() {
         </a>
       </li>
       <li className="nav-item">
-        <a id="wd-k" href="https://kanbas-node-server-app-1-9tgc.onrender.com" className="nav-link">
+        <a id="wd-k" href="https://kanbas-node-server-app-2-evgz.onrender.com" className="nav-link">
           New Node Server on Render.com
         </a>
       </li>
